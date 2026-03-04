65f465dab469984c8e1d62bf277192649fc2d081
Real-Blakelf <real.blakefranks@gmail.com>
2026-03-03 20:53:09 -0500

    Update LeaderBoard.java

---
+++ src/main/java/com/model/LeaderBoard.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import java.util.List;
+import java.util.HashMap;
+
+public class Leaderboard {
+
+    public List<User> getTopPerformers(int limit) {
+        return null;
+    }
+
+    public HashMap<String, Integer> getStats() {
+        return null;
+    }
+
+}
