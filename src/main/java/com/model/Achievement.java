66f079d75d513c91d8d0c1fcff0bb6f6165bcb83
Real-Blakelf <real.blakefranks@gmail.com>
2026-03-03 20:52:36 -0500

    Update Achievement.java

---
+++ src/main/java/com/model/Achievement.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import java.util.UUID;
+
+public class Achievement {
+
+    private UUID id;
+    private String name;
+    private String criteria;
+    private String badgeCategory;
+
+}
